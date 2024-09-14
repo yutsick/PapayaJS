3164363e3160d6a6dd5a1ccb6ed1624f2208e303
Taras Yurchyshyn <yts.prof@gmail.com>
2024-09-14 16:27:35 +0300

    made 12.09 changes and bluesnap payment fields implemented

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,118 @@
+// bluesnap payment fields
+const token = 'b44b553f82290ad50eaec4a9ab333c810b57eb2e00075eb061282694ce77d74f_';
+
+function changeImpactedElement(tagId, removeClass = "", addClass = "") {
+
+  const elements = document.querySelectorAll(`[data-bluesnap="${tagId}"]`);
+
+
+  elements.forEach(element => {
+
+    if (removeClass) {
+      element.classList.remove(removeClass);
+    }
+
+    if (addClass) {
+      element.classList.add(addClass);
+    }
+  });
+}
+
+bluesnap.hostedPaymentFieldsCreate({
+  token: token,
+  onFieldEventHandler: {
+      onFocus: function (tagId) {
+          changeImpactedElement(tagId, "", "hosted-field-focus");
+      },
+      onBlur: function (tagId) {
+          changeImpactedElement(tagId, "hosted-field-focus");
+      },
+      onError: function (tagId, errorCode, errorDescription, eventOrigin) {
+          const helpElement = document.querySelector(`#${tagId}-error`);
+          if (helpElement) {
+              helpElement.textContent = `${errorCode} - ${errorDescription} - ${eventOrigin}`;
+          }
+      },
+      onType: function (tagId, cardType, cardData) {
+          const cardLogo = document.getElementById("card-logo");
+          if (cardLogo) {
+              const img = cardLogo.querySelector("img");
+              if (img) {
+                  img.src = cardUrl[cardType];
+              }
+          }
+
+      },
+      onValid: function (tagId) {
+          const helpElement = document.getElementById(tagId + "-help");
+          if (helpElement) {
+              helpElement.textContent = "";
+          }
+      }
+  },
+  hostedFields: {
+      cardNumber: {
+          selector: '#card-number',
+          placeholder: 'Card Number'
+      },
+      expirationDate: {
+          selector: '#card-expiration',
+          placeholder: 'MM / YY'
+      },
+      securityCode: {
+          selector: '#card-cvv',
+          placeholder: 'CVV'
+      }
+  },
+  style:{
+    "input": {
+      "font-size": "1rem",
+      "font-family": "Poppins,sans-serif",
+      "color": "#212529",
+      "display": "block",
+      'height': '40px',
+    },
+
+    "::placeholder": {
+      "color": "#D1D3D3",
+      "font-size": "clamp(14px, 3vw,16px)",
+      "font-family": "Poppins,sans-serif",
+    },
+    
+  },
+    ccnPlaceHolder: "Enter Card Number",
+    cvvPlaceHolder: "***",
+    expPlaceHolder: "MM/YY"
+});
+
+document.querySelector('#bluesnap-submit').addEventListener('click', function (event) {
+
+  bluesnap.hostedPaymentFieldsSubmitData(function(callback) {
+    if (callback.error) {
+      const errorArray = callback.error;
+      errorArray.forEach(error => {
+        const errorElement = document.getElementById(error.tagId + "-error");
+        if (errorElement) {
+          errorElement.textContent = `${error.errorCode} - ${error.errorDescription}`;
+        }
+      });
+    } else {
+      const cardData = callback.cardData;
+      alert(
+        `Card Type: ${cardData.ccType}\n` +
+        `Last 4 Digits: ${cardData.last4Digits}\n` +
+        `Issuing Country: ${cardData.issuingCountry}\n` +
+        `Is Regulated Card: ${cardData.isRegulatedCard}\n` +
+        `Card Sub Type: ${cardData.cardSubType}\n` +
+        `Bin Category: ${cardData.binCategory}\n` +
+        `Exp: ${cardData.exp}\n` +
+        `after that I can call final submit`
+      );
+    }
+  });
+});
+
+// End of bluesnap payment fields
 
 const paymentButtons = document.querySelectorAll('.pay-apple, .pay-google, .pay-paypal');
 
@@ -83,3 +198,11 @@ function checkFormFields() {
       alert('Form is valid!');
     }
   };
+// Change Total price on tab click
+document.querySelectorAll('[data-bs-toggle]').forEach(function (toggle) { 
+    toggle.addEventListener('click', function () {
+        const price = this.querySelector('.price-tab__price').textContent;
+        document.querySelector('.checkout__total .price').textContent = price;
+        console.log(price);
+    });
+});
